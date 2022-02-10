3fc90419780d4bf9de33474a384b16ee9c32ecda
Anjar Febrianto <anxfeb@gmail.com>
2022-02-10 10:08:14 +0700

    update index page

---
+++ components/DarkModeButton/index.tsx
@@ -0,0 +1,21 @@
+import { useTheme } from 'next-themes';
+import {SunFill, MoonFill} from '@emotion-icons/bootstrap';
+
+const DarkModeButton = () => {
+  const { theme, setTheme } = useTheme();
+  const handleClick = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <>
+      <button type="button" className="px-2 rounded-lg" onClick={handleClick} title="Dark Mode Button">
+        {theme === 'dark'
+          ? <SunFill size={26} />
+          : <MoonFill size={26} className="text-gray-700" />}
+      </button>
+    </>
+  );
+};
+
+export default DarkModeButton;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import dynamic from 'next/dynamic';
+import { useTheme } from 'next-themes';
 
+const DarkModeButton = dynamic(() => import('components/DarkModeButton'), {
+  ssr: false,
+});
 const Home: NextPage = () => {
+  const { theme, setTheme } = useTheme();
+
   return (
     <div>
       <Head>
@@ -11,54 +18,48 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p>
-          Get started by editing{' '}
-          <code >pages/index.tsx</code>
-        </p>
-
+      <main className="flex min-h-screen flex-col items-center justify-center text-center">
+        
         <div>
-          <a href="https://nextjs.org/docs" >
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <Image src={theme === 'dark' ? '/nextjs-light.svg' : '/nextjs-dark.svg'} alt="Nextjs Logo" width={200} height={100} />
+        <Image src={theme === 'dark' ? '/tailwindcss-light.svg' : '/tailwindcss-dark.svg'} alt="Nextjs Logo" width={300} height={100} />
 
-          <a href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          
         </div>
+        <h1 className="text-6xl font-bold mb-10">
+          Next.js + Tailwindcss Starter
+        </h1>
+        
+        <div className="mb-5">
+          <span className="mb-2">A starter template for your next project with&nbsp;
+              <a className="hover:text-sky-600" href="https://nextjs.org">Next.js</a>,&nbsp;
+              <a className="hover:text-sky-600" href="https://tailwindcss.com">Tailwindcss</a>,&nbsp;
+              <a className="hover:text-sky-600" href="https://www.typescriptlang.org/"></a>TypeScript,&nbsp;
+              <a className="hover:text-sky-600" href="https://emotion-icons.dev/">Emotion Icons</a>,&nbsp;
+              <a className="hover:text-sky-600" href="https://github.com/pacocoursey/next-themes">next-themes</a> and pre-configured with 
+              <a className="hover:text-sky-600" href="https://eslint.org">Eslint</a>,
+              <a className="hover:text-sky-600" href="https://typicode.github.io/husky/">Husky</a>, and&nbsp;
+              <a className="hover:text-sky-600" href="https://github.com/okonet/lint-staged">Lint-staged</a> 
+          </span>
+        
+        </div>
+        <div className="mb-5">
+          <span className=" mb-2">Bonus: Dark-mode button<DarkModeButton /></span>
+         
+        </div>
+
+        <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fanjar%2Fnextjs-tailwind-starter&project-name=nextjs-tailwind&repo-name=nextjs-tailwind"><img src="https://vercel.com/button" alt="Deploy with Vercel"/></a>
       </main>
 
-      <footer>
+      <footer className="absolute w-full bottom-1 items-center justify-center text-center">
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          &copy; 2022 <a href="https://anjar.my.id">Anjar Febrianto</a>, Powered by{' '}
           <span>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src={theme === 'dark' ? '/vercel-light.svg' : '/vercel-dark.svg'} alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
